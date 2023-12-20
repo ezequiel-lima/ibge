d13a00f923225bcce03b68ba9cbcff228f82f569
ezequiel-lima <joao.lima94@fatec.sp.gov.br>
2023-12-20 17:05:09 -0300

    config de banco

--- IBGE/Data/AppDbContext.cs
+++ IBGE/Data/AppDbContext.cs
@@ -11,7 +11,7 @@ namespace IBGE.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server = DESKTOP-HP2UQRQ\\SQLEXPRESS; Database = IbgeDataBase; Trusted_Connection = True; Encrypt = False");
+            optionsBuilder.UseSqlServer("Server=tcp:desafioblazor.database.windows.net,1433;Initial Catalog=DesafioDataBase;Persist Security Info=False;User ID=desafioblazor;Password=Quiel@2021#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
